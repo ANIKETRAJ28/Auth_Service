0ae6135eb57fa58534eef928ea402f105c9aa206
Aniket <aniketraj28042003@gmail.com>
2024-02-01 19:54:11 +0530

    roles and user_roles table

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const app = express();
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
 const apiRoutes = require("./routes/index");
+const db = require("./models/index");
+
 // const UserRepository = require("./repository/user-repository");
-const UserService = require("./services/user-service");
+// const UserService = require("./services/user-service");
 
 const serverSetup = () => {
 
@@ -14,6 +16,9 @@ const serverSetup = () => {
     app.use("/api", apiRoutes);
 
     app.listen(PORT, async () => {
+        if(process.env.DB_SYNC) {
+            db.sequelize.sync({alter: true});
+        }
         console.log(`server started at port ${PORT}`);
         // const repository = new UserRepository();
         // const response = await repository.getById(5);
